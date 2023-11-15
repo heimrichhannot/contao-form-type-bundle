73c9435d3b51452f1b27cf1415c73cdb59b4170a
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2023-11-15 17:46:45 +0100

    Added: get FormType from FormTypeCollection

--- src/FormType/FormTypeCollection.php
+++ src/FormType/FormTypeCollection.php
@@ -2,11 +2,13 @@
 
 namespace HeimrichHannot\FormTypeBundle\FormType;
 
+use Contao\Form;
+
 class FormTypeCollection
 {
     private array $types = [];
 
-    public function addType(FormTypeInterface $type): void
+    public function addType(AbstractFormType|FormTypeInterface $type): void
     {
         $this->types[$type->getType()] = $type;
     }
@@ -16,8 +18,16 @@ class FormTypeCollection
         return $this->types;
     }
 
-    public function getType(string $type): ?FormTypeInterface
+    public function getType(string $type): AbstractFormType|FormTypeInterface|null
     {
         return $this->types[$type] ?? null;
+    }
+
+    public function getTypeOfForm(Form $form): AbstractFormType|FormTypeInterface|null
+    {
+        if ($form->formType && $formType = $this->getType($form->formType)) {
+            return $formType;
+        }
+        return null;
     }
 }
